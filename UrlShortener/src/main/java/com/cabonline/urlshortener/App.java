5631197424ec68419973bcf231adaf0b147a9722
erlandi <34107514+erlandi@users.noreply.github.com>
2017-11-30 05:59:44 +0100

    Update App.java

--- UrlShortener/src/main/java/com/cabonline/urlshortener/App.java
+++ UrlShortener/src/main/java/com/cabonline/urlshortener/App.java
@@ -6,14 +6,9 @@ import java.util.Map;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
-/**
- * Hello world!
- *
- */
 @SpringBootApplication
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
 		Map<String, Object> props = new HashMap<String, Object>();
 		props.put("server.port", 8081);
 		new SpringApplicationBuilder(App.class).properties(props)
